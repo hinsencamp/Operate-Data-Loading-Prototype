3770f48aad0a856e2baf5f6e1654daf150110013
Fabian Hinsenkamp <fabian.hinsenkamp@online.de>
2019-05-21 19:10:24 +0200

    Add Filter & Diagram
    Filter are publishing updates when users change them. 
    Diagram subscribes to changed filters and updates accordingly

--- index.js
+++ index.js
@@ -12,7 +12,6 @@ function DataManager(){
   this.subscribers = [];
 }
 
-
 DataManager.prototype.subscribe = function(topics, callback){
   this.subscribers.push({ topics, callback });
 }
@@ -26,24 +25,52 @@ DataManager.prototype.publish = function(topic, value){
 
 }
 
-function Filter(){
+function Filter(dataManager){
   // filter change --> new Diagram
+  this.appliedFilters = {
+    diagramName: "someWorkflow",
+    incidents: true
+  }
+  this.dataManager = dataManager;
 }
 
-function Diagram(){
+Filter.prototype.changeAppliedFilters = function(key, newValue){
+  this.appliedFilters = { ...this.appliedFilters, [key]: newValue}
+  this.dataManager.publish('filters', this.appliedFilters)
+}
+
+function Diagram(dataManager){
   // new Diagram loaded --> new Overlays need to be loaded
+  this.diagram = { name: ""};
+  this.dataManager = dataManager;
+}
+
+
+Diagram.prototype.subscribeForUpdates = function(){
+  this.dataManager.subscribe(['filters'], (newFilters)=>{
+    if(newFilters.diagramName != this.diagram.name){
+
+      // Add fetching Data here
+
+      this.diagram = {...this.diagram,
+      name: newFilters.diagramName
+      };
+      console.log('Diagram was updated:', this.diagram);
+    }  
+});
 }
 
 function Statistics(){
   // filter change --> new statistics are loaded
 }
 
-
 /** Setup */ 
 const dataM = new DataManager();
 
-dataM.subscribe(['filters', 'statistics', 'diagram'], (returnedData)=>{
-console.log('I have got:', returnedData);
-});
 
-dataM.publish('diagram', 'diagramValue');
+const filter = new Filter(dataM);
+const diagram = new Diagram(dataM);
+diagram.subscribeForUpdates();
+
+// add new 
+filter.changeAppliedFilters('diagramName', "ComplexWorkflow");
